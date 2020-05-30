bc7cf5362d89248eb21f385c0687253e943b4310
EvanPutnam <emput1998@gmail.com>
2020-05-30 02:46:40 -0400

    Adding basic front page template

---
+++ markdown/README.md
@@ -0,0 +1,4 @@
+# Markdown Edit Process
+
+The process for editing markdown files for the website is pretty simple.  Create the markdown file inside the markdown_files folder, place any images inside the image folder,
+and then use the generate_article.py .bat file in the util folder.
--- src/index.js
+++ src/index.js
@@ -51,19 +51,41 @@ class ArticleSummaryBlock extends React.Component {
 class ArticleSummarySection extends React.Component {
     render() {
         return (
-            <div className="Article-Summary-Section">
-                <ArticleSummaryBlock article={loaded_articles["Articles"][0]}/>
-                <ArticleSummaryBlock article={loaded_articles["Articles"][1]}/>
-                <ArticleSummaryBlock article={loaded_articles["Articles"][2]}/>
+            <div className="Article-Summary">
+                <div class="w3-card-4 w3-margin w3-white">
+                    <div class="w3-container">
+                        <h3><b>{this.props.article['title']}</b></h3>
+                        <h5><span class="w3-opacity">{this.props.article['date']}</span></h5>
+                    </div>
+                    <div class="w3-container">
+                        <p>{this.props.article['summary']}</p>
+                        <div class="w3-row">
+                            <div class="w3-col m8 s12">
+                            <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
             </div>
         );
     }
+}
 
-
+class RelevantArticleContainer extends React.Component {
+    render() {
+        return (
+            <div className="Article-Summary-Container">
+                <ArticleSummarySection article={loaded_articles["Articles"][0]}/>
+                <ArticleSummarySection article={loaded_articles["Articles"][1]}/>
+                <ArticleSummarySection article={loaded_articles["Articles"][2]}/>
+            </div>
+        );
+    };
 }
 
 
 ReactDOM.render(
-    <ArticleSummarySection />,
-    document.getElementById('root')
+    <RelevantArticleContainer />,
+    document.getElementById('articlePosts')
 );
