1ae825ad1c8d9ee0aa5636980c4a88f8a979a4d8
EvanPutnam <emput1998@gmail.com>
2020-05-30 02:47:26 -0400

    Cleaning up file

--- src/index.js
+++ src/index.js
@@ -5,49 +5,6 @@ import './index.css';
 
 const loaded_articles = require("./articles/projects.json")
 
-class ShoppingList extends React.Component {
-    render() {
-        return (
-        <div className="shopping-list">
-            <h1>Shopping List for {this.props.name}</h1>
-            <ul>
-            <li>Instagram</li>
-            <li>WhatsApp</li>
-            <li>Oculus</li>
-            </ul>
-        </div>
-        );
-    }
-}
-
-
-class Article extends React.Component {
-
-    render() {
-        return (
-            <div className="Article-Summary-Block">
-                <h1>{this.props.article['title']}</h1>
-                <div dangerouslySetInnerHTML={this.createMarkup()}></div>
-            </div>
-        );
-    }
-    createMarkup(){
-        return {__html: this.props.article['text']};
-    }
-}
-
-class ArticleSummaryBlock extends React.Component {
-    render() {
-        return (
-            <div className="Article-Summary-Block">
-                <h1>{this.props.article['title']}</h1>
-                <p>{this.props.article['summary']}</p>
-                <Article article={this.props.article}/>
-            </div>
-        );
-    }
-}
-
 class ArticleSummarySection extends React.Component {
     render() {
         return (
