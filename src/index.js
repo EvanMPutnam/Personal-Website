32aac137cc2be6819c159b93f8dbe23098b1cdc0
EvanPutnam <emput1998@gmail.com>
2020-05-31 00:52:51 -0400

    Ability to switch between posting list and real post is finished

--- src/index.js
+++ src/index.js
@@ -5,7 +5,50 @@ import './index.css';
 
 const loaded_articles = require("./articles/projects.json")
 
+
+class Article extends React.Component {
+    constructor (props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="Article">
+                <div class="w3-card-4 w3-margin w3-white">
+                    <div class="w3-container">
+                        <h3><b>{this.props.article['title']}</b></h3>
+                        <h5><span class="w3-opacity">{this.props.article['date']}</span></h5>
+                    </div>
+                    <div class="w3-container">
+                        <div dangerouslySetInnerHTML={ {__html: this.props.article['text']} }></div>
+                        <div class="w3-row">
+                            <div class="w3-col m8 s12">
+                            <p><button class="w3-button w3-padding-large w3-white w3-border" onClick={this.handleClick}><b>« BACK</b></button></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+            </div>
+        );
+    }
+
+    handleClick() {
+        ReactDOM.render(
+            <RelevantArticleContainer />,
+            document.getElementById('articlePosts')
+        );
+    }
+}
+
 class ArticleSummarySection extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
     render() {
         return (
             <div className="Article-Summary">
@@ -18,13 +61,21 @@ class ArticleSummarySection extends React.Component {
                         <p>{this.props.article['summary']}</p>
                         <div class="w3-row">
                             <div class="w3-col m8 s12">
-                            <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+                            <p><button class="w3-button w3-padding-large w3-white w3-border" onClick={this.handleClick}><b>READ MORE »</b></button></p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <hr/>
             </div>
+        );
+    }
+
+    handleClick() {
+        const properties = this.props.article;
+        ReactDOM.render(
+            <Article article={properties} />,
+            document.getElementById('articlePosts')
         );
     }
 }
@@ -46,3 +97,4 @@ ReactDOM.render(
     <RelevantArticleContainer />,
     document.getElementById('articlePosts')
 );
+
